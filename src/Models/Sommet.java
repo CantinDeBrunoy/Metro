578feb1f61a827c0fd70967b57a77bdcd29ef0df
CantinDeBrunoy <cantin.roquier@gmail.com>
2022-10-08 16:00:35 +0200

    feat(main): creation de sommets basiques

--- src/Models/Sommet.java
+++ src/Models/Sommet.java
@@ -3,8 +3,9 @@ package Models;
 public class Sommet {
 	private String nom;
 	private Boolean estMarque = false;	
-	
-	public Sommet (String nom) {
+	private int id;
+	public Sommet (String nom, int id) {
 		this.nom = nom;
+		this.id = id;
 	}
 }
--- src/metro/metro.java
+++ src/metro/metro.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
+import Models.Sommet;
+
 
 public class metro {
 	public static void main(String[] args) {
@@ -22,7 +24,11 @@ public class metro {
 		try(FileInputStream fis = new FileInputStream(file)){
 			Scanner sc = new Scanner(fis);
 			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+				String[] ligne = sc.nextLine().split(" ");
+				if(ligne[0].equals("V")) {
+					Sommet sommet = new Sommet(ligne[2],Integer.parseInt(ligne[1]));
+				}
+				
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
